4757273baf190e17da894bff3a006f679958df99
Vinay200305 <148442500+Vinay200305@users.noreply.github.com>
2025-02-19 21:30:49 +0530

    Add files via upload

---
+++ JS/2.Variables_and_datatypes/Lab_assignment/1_Data_type.js
@@ -0,0 +1,21 @@
+let age = 25;
+let price = 9.99;
+let greeting = 'Hello, World!';
+let isLoggedIn = false;
+let emptyValue = null;
+let notDefined;
+let bigNumber = 123456789012345678901234567890n;
+let person = {
+    name: "Alice",
+    age: 30,
+    isStudent: false
+};
+
+console.log(age)
+console.log(price)
+console.log(greeting)
+console.log(isLoggedIn)
+console.log(emptyValue)
+console.log(notDefined)
+console.log(bigNumber)
+console.log(person)
---
+++ JS/2.Variables_and_datatypes/Lab_assignment/2_Log_variable.js
@@ -0,0 +1,11 @@
+let name = "Kedar";
+let age = 25;
+let isActive = true;
+let user = { name: "Kedar", age: 25 };
+let numbers = [1, 2, 3, 4];
+
+console.log("name:", name, "Type:", typeof name);
+console.log("age:", age, "Type:", typeof age);
+console.log("isActive:", isActive, "Type:", typeof isActive);
+console.log("user:", user, "Type:", typeof user);
+console.log("numbers:", numbers, "Type:", typeof numbers);
